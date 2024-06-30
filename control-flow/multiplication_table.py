1404787e5e07d5e2ade8f299bddfbb04a645bf5d
Rene Arem <renearem@gmail.com>
2024-06-30 16:41:22 +0300

    multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+# multiplication_table.py
+
+# Prompt User for a Number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and Print the Multiplication Table
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
